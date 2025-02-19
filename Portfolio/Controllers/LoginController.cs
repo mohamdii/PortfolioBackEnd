07e624c84b263022770d2c045558289091870768
mohamdii <122834211+mohamdii@users.noreply.github.com>
2025-02-19 17:29:43 +0200

    Add project files.

---
+++ Portfolio/Controllers/LoginController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio.Controllers;
+
+public class LoginController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+    public IActionResult Register()
+    {
+        return View();
+    }
+}
---
+++ Portfolio/Models/Login.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages.Infrastructure;
+using System.ComponentModel.DataAnnotations;
+
+namespace Portfolio.Models
+{
+    public class Login
+    {
+        [Required]
+        [Display(Name ="Login using username or email")]
+        public string UserNameOrEmail { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        public bool RememberMe { get; set; }
+    }
+}
